660a05905a8f69abe33856edce83219ac90e0c56
Anthony Rosinetti <10341309+anthonyrosinetti@users.noreply.github.com>
2025-05-27 17:18:06 +0200

    Create analysis_orders_months_current_filtered_sku.sql

---
+++ SQLToolbox/custom_queries/LCDA/analysis_orders_months_current_filtered_sku.sql
@@ -0,0 +1,43 @@
+WITH layer1 AS (
+  SELECT
+  	  DATE_TRUNC(date,MONTH) AS month,
+  	  store,
+      manufacturer,
+      surtype,
+      type,
+      suppliers,
+      brand,
+      product_sku,
+      product_name,
+      ROUND(SUM(item_total_revenue),0) AS monthly_ca,
+      COUNT(DISTINCT order_id) AS monthly_orders,
+      SUM(item_total_quantity) AS monthly_sold_units,
+      AVG(item_margin) AS monthly_margin
+  FROM
+      `reporting-boryl-lcda.analysis.analysis_orders`
+  WHERE
+      date between PARSE_DATE('%Y%m%d',  @DS_START_DATE) AND PARSE_DATE('%Y%m%d',  @DS_END_DATE)
+      and
+      product_sku IN (
+        SELECT
+        sku
+        FROM
+        `reporting-boryl-lcda.config.filtered_sku`
+      )	  
+  GROUP BY
+      DATE_TRUNC(date,MONTH),
+      store,
+      manufacturer,
+      surtype,
+      type,
+      suppliers,
+      brand,
+      product_sku,
+      product_name
+)
+
+SELECT
+  *,
+  DENSE_RANK() OVER (ORDER BY month) - 1 AS row_number_group
+FROM
+	layer1
