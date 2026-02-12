9d8ef5d678f2264fc9567e8dd395073b28476299
Anthony Rosinetti <10341309+anthonyrosinetti@users.noreply.github.com>
2026-02-12 17:37:34 +0100

    Create stg_google_ads_campaign.sql

---
+++ SQLToolbox/custom_queries/Dougs/stg_google_ads_campaign.sql
@@ -0,0 +1,30 @@
+config {
+    type: "incremental",
+    schema: "preanalysis",
+    partitionBy: "date_last_refresh",
+    requirePartitionFilter: false,
+    protected: true
+}
+
+-- Pre-operations
+pre_operations {
+    ${when(incremental(),
+    `DELETE FROM ${self()} WHERE DATE(_DATA_DATE) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 35 DAY) AND CURRENT_DATE();`
+    )}
+}
+
+SELECT
+    _DATA_DATE AS date_last_refresh,
+    CAST(customer_id AS STRING) AS customer_id,
+    CAST(campaign_id AS STRING) AS campaign_id,
+    campaign_name,
+    campaign_advertising_channel_type AS advertising_channel_type,
+    campaign_advertising_channel_sub_type AS advertising_channel_sub_type
+FROM
+    ${ref("ads_Campaign_9369677276")}
+WHERE
+    TRUE
+    -- Due to important data volume in some Google Ads table, using incremental refresh strategy ; data is considered as stable after around 30 days
+    ${when(incremental(),
+    `AND DATE(_DATA_DATE) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 35 DAY) AND CURRENT_DATE()`
+    )}
