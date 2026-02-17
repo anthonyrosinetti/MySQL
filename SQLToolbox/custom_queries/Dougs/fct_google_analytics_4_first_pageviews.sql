24d5321e3efe790f91b987e605dcab801fea38b7
Anthony Rosinetti <10341309+anthonyrosinetti@users.noreply.github.com>
2026-02-17 14:51:44 +0100

    Update fct_google_analytics_4_first_pageviews.sql

--- SQLToolbox/custom_queries/Dougs/fct_google_analytics_4_first_pageviews.sql
+++ SQLToolbox/custom_queries/Dougs/fct_google_analytics_4_first_pageviews.sql
@@ -29,7 +29,7 @@ WITH ga4_events AS (
 ),
 
 first_pageviews_calculation AS (
-    SELECT
+    SELECT DISTINCT
         event_timestamp AS first_page_timestamp,
         user_pseudo_id,
         page_location AS first_page_location,
@@ -38,25 +38,48 @@ first_pageviews_calculation AS (
         ga4_events
     WHERE
         event_name LIKE "%page%view%"
+        AND
+        REGEXP_EXTRACT(page_location, r"^(?:https?://)?([^/?#]+)") = 'www.dougs.fr'        
     QUALIFY
         ROW_NUMBER() OVER (
             PARTITION BY user_pseudo_id
             ORDER BY event_timestamp ASC
         ) = 1
+),
+
+first_pageviews_decoding AS (
+    SELECT
+        DATE(first_page_timestamp) AS date_report,
+        first_page_timestamp,
+        user_pseudo_id,
+        -- Removing protocol as well as URL parameters / anchors
+        REGEXP_EXTRACT(first_page_location, r"^(?:https?://)?[^/]+(/[^?#]*)") AS first_page,
+        REGEXP_EXTRACT(first_page_referrer, r"^(?:https?://)?[^/]+(/[^?#]*)") AS first_referrer
+    FROM
+        first_pageviews_calculation
 )
 
 SELECT
-    DATE(first_page_timestamp) AS date_report,
-    first_page_timestamp,
-    user_pseudo_id,
-    -- Removing protocol as well as URL parameters / anchors
-    REGEXP_EXTRACT(first_page_location, r"^(?:https?://)?[^/]+(/[^?#]*)") AS first_page,
-    REGEXP_EXTRACT(first_page_referrer, r"^(?:https?://)?[^/]+(/[^?#]*)") AS first_referrer
+    *,
+    CASE
+        WHEN STARTS_WITH(first_page,'/blog/') THEN 'Blog'
+        WHEN STARTS_WITH(first_page,'/webinar') THEN 'Webinar'
+        WHEN STARTS_WITH(first_page,'/videos') THEN 'Video'
+        WHEN STARTS_WITH(first_page,'/guide') THEN 'Guide'
+        WHEN STARTS_WITH(first_page,'/ressources/') THEN 'Ressource'
+        WHEN STARTS_WITH(first_page,'/temoignage') THEN 'Témoignage'
+        WHEN STARTS_WITH(first_page,'/outils-simulation/') THEN 'Outil de simulation'
+        WHEN STARTS_WITH(first_page,'/expert-comptable/') THEN 'Expert-comptable'
+        WHEN STARTS_WITH(first_page,'/parten') THEN 'Partenariat'
+        WHEN STARTS_WITH(first_page,'/cre') THEN 'Création'
+        WHEN STARTS_WITH(first_page,'/facturation-electronique/') THEN 'Facturation électronique'
+        ELSE '-'
+    END AS first_page_category
 FROM
-    first_pageviews_calculation
+    first_pageviews_decoding
 WHERE
     TRUE
     {% if is_incremental() %}
     -- Removing cross day session from day before date range
-    AND DATE(session_start_timestamp) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 DAY) AND CURRENT_DATE()
-    {% endif %}
+    AND date_report BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 DAY) AND CURRENT_DATE()
+    {% endif %}    
