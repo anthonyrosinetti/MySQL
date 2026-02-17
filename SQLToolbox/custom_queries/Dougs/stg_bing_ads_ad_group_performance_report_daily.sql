518e469ee4e3f166f7cc84c6a3b6542860d95eb5
Anthony Rosinetti <10341309+anthonyrosinetti@users.noreply.github.com>
2026-02-17 14:30:43 +0100

    Create stg_bing_ads_ad_group_performance_report_daily.sql

---
+++ SQLToolbox/custom_queries/Dougs/stg_bing_ads_ad_group_performance_report_daily.sql
@@ -0,0 +1,12 @@
+SELECT
+    _airbyte_extracted_at AS timestamp_last_refresh,
+    TimePeriod AS date_report,
+    CAST(AccountId AS STRING) AS account_id,
+    CAST(CampaignId AS STRING) AS campaign_id,
+    CAST(AdGroupId AS STRING) AS ad_group_id,
+    DeviceType AS device_type,
+    Impressions AS impressions,
+    Clicks AS clicks,
+    Spend AS spend
+FROM
+    {{ source('bronze_airbyte_bing_ads', 'ad_group_performance_report_daily') }}
